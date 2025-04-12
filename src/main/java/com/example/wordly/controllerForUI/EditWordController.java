14a98e2428eb9dc41e79bd4dfa27df41a02e5105
Pham Thai <24022449@vnu.edu.vn>
2025-04-12 16:34:51 +0700

    them text area

--- src/main/java/com/example/wordly/controllerForUI/EditWordController.java
+++ src/main/java/com/example/wordly/controllerForUI/EditWordController.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
