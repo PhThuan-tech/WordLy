6c6e40a251694e912fe9405eef119d079aa156e7
PhThuan-Tech <24022461@vnu.edu.vn>
2025-04-20 07:03:45 +0700

    Fixed all scence and add more icon

--- src/main/java/com/example/wordly/controllerForUI/MainController.java
+++ src/main/java/com/example/wordly/controllerForUI/MainController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.media.AudioClip;
 
 public class MainController extends BaseController {
     @FXML
@@ -14,6 +15,8 @@ public class MainController extends BaseController {
 
     @FXML
     public Button features;
+
+    private AudioClip clickSound;
 
     /**
      * Hàm khởi tạo CSS vào trong MainController.
@@ -26,6 +29,7 @@ public class MainController extends BaseController {
                         .getResource("/com/example/wordly/styles/mainsce.css").toExternalForm());
             }
         }
+        clickSound = new AudioClip(getClass().getResource("/com/example/wordly/audio/TrollButton.mp3").toString());
     }
 
     // Các Hàm chuyển đổi giữa các giao diện.
@@ -35,5 +39,16 @@ public class MainController extends BaseController {
 
     public void handleGoToUsing(ActionEvent actionEvent) {
         switchScene(actionEvent, "/com/example/wordly/View/UsingMehodView.fxml");
+    }
+
+
+    @FXML
+    public void HandlePlaysound(ActionEvent actionEvent) {
+        // Phát âm thanh
+        if (clickSound != null) {
+            clickSound.play();
+        } else {
+            System.out.println("Âm thanh chưa được load!");
+        }
     }
 }
