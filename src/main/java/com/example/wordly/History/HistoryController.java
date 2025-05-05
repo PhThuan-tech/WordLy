52aea6f3981facd36fa10e00920d3ccc8c9a163e
tbmdemi <24022413@vnu.edu.vn>
2025-05-05 13:59:18 +0700

    test : nice

--- src/main/java/com/example/wordly/History/HistoryController.java
+++ src/main/java/com/example/wordly/History/HistoryController.java
@@ -262,9 +262,6 @@ public class HistoryController extends BaseController implements Initializable {
     @FXML public void GoToAdvanceFeature(ActionEvent actionEvent) {
         switchScene(actionEvent, "/com/example/wordly/View/Advance_Features.fxml");
     }
-    @FXML public void handleGoToChat(ActionEvent actionEvent) {
-        switchScene(actionEvent, "/com/example/wordly/View/ChatView.fxml");
-    }
 
     /**
      * TAT EXECUTOR
